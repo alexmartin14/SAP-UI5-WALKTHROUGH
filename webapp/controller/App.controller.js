b3e1675edeef2d401ecca079d9a5e7a16f1be11e
alexmartin14 <alexkmartin14@gmail.com>
2021-09-03 15:23:25 +0100

    Translatable texts
    Moving the texts of our UI to a separate resource folder

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,34 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      // onInit is one of SAPUI5's lifecycle methods
-      onInit : function () {
+     onInit : function () {
          // set data model on view
          var oData = {
             recipient : {
                name : "World"
             }
          };
-         // initiate a JSON model 
          var oModel = new JSONModel(oData);
          this.getView().setModel(oModel);
+         // set i18n model on view
+         var i18nModel = new ResourceModel({
+            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+         });
+         // named models are used when you need to have several models in parallel
+         this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello : function () {
-         MessageToast.show("Hello World");
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
       }
    });
 });
