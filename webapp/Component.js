3fa488ce14aab008fc3b50421ccec697d7fea251
alexmartin14 <alexkmartin14@gmail.com>
2021-09-20 12:59:25 +0100

    Routing, Navigation, Parameters and Routing Back

--- webapp/Component.js
+++ webapp/Component.js
@@ -29,6 +29,8 @@ sap.ui.define([
 			this.rootControlLoaded().then(function() {
 				this._helloDialog = new HelloDialog(this.getRootControl());
 			}.bind(this));
+			// create the views based on the url/hash
+			this.getRouter().initialize();
 		},
 
 		exit : function () {
